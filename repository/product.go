fdfc2848ea1649500bbd088a3ad71ac5e45abdc0
Leo <1906322800@qq.com>
2022-02-15 10:58:08 +0800

    同事根据sort和remaining_count desc排序

--- repository/product.go
+++ repository/product.go
@@ -20,7 +20,7 @@ type ProductRepository struct {
 
 func (p ProductRepository) ProductList() ([]Product.Product, error) {
 	var products []Product.Product
-	if err := app.DB.Table("product_item").Where("active = ?", "true").Order("sort desc").Find(&products).Error; err != nil {
+	if err := app.DB.Table("product_item").Where("active = ?", "true").Order("sort desc,remaining_count desc").Find(&products).Error; err != nil {
 		return nil, err
 	}
 	return products, nil
