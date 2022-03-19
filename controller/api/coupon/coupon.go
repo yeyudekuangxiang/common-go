fe4a99aed3237eb06a20d3f9ccc239172a2e8101
Leo <1906322800@qq.com>
2022-03-19 11:32:22 +0800

    券码记录修改

--- controller/api/coupon/coupon.go
+++ controller/api/coupon/coupon.go
@@ -12,7 +12,7 @@ type CouponController struct {
 
 func (CouponController) CouponListOfOpenid(c *gin.Context) (gin.H, error) {
 	openid := c.Query("openid")
-	list, err := service.DefaultCouponService.CouponListOfOpenid(openid, "80defb4f-f002-442f-b3a8-6c28a04ba814,evcard0point")
+	list, err := service.DefaultCouponService.CouponListOfOpenid(openid, []string{"80defb4f-f002-442f-b3a8-6c28a04ba814", "evcard0point"})
 
 	return gin.H{
 		"records": list,
