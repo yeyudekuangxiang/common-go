dd90c36e9ec5e30df0814d1de3fe92ffb2847099
neiljin <neil.jin@miotech.com>
2022-03-22 13:00:22 +0800

    feat:暂时直接从topic表返回数据

--- controller/api/topic.go
+++ controller/api/topic.go
@@ -21,6 +21,37 @@ func (TopicController) List(c *gin.Context) (gin.H, error) {
 
 	user := util.GetAuthUser(c)
 
+	list, total, err := service.DefaultTopicService.GetTopicDetailPageList(repository.GetTopicPageListBy{
+		ID:         form.ID,
+		TopicTagId: form.TopicTagId,
+		Offset:     form.Offset(),
+		Limit:      form.Limit(),
+		UserId:     user.ID,
+	})
+	if err != nil {
+		return nil, err
+	}
+	ids := make([]int64, 0)
+	for _, item := range list {
+		ids = append(ids, item.Id)
+	}
+	app.Logger.Infof("GetTopicDetailPageListByFlow user:%d form:%+v ids:%+v", user.ID, form, ids)
+
+	return gin.H{
+		"list":     list,
+		"total":    total,
+		"page":     form.Page,
+		"pageSize": form.PageSize,
+	}, nil
+}
+func (TopicController) List1(c *gin.Context) (gin.H, error) {
+	form := GetTopicPageListForm{}
+	if err := util.BindForm(c, &form); err != nil {
+		return nil, err
+	}
+
+	user := util.GetAuthUser(c)
+
 	list, total, err := service.DefaultTopicService.GetTopicDetailPageListByFlow(repository.GetTopicPageListBy{
 		ID:         form.ID,
 		TopicTagId: form.TopicTagId,
