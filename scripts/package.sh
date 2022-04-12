4865fa1f8c2456ab96b4aaf33fb1bcdb0aac4d33
neiljin <neil.jin@miotech.com>
2022-04-13 00:51:52 +0800

    feat:helm部署项目以及服务号网页授权登陆

--- scripts/package.sh
+++ scripts/package.sh
@@ -7,8 +7,16 @@ pwd
 # 参数1要打包的目录名 mp2c 分支名 v1.1.1 develop release-
 env
 tag=$CI_COMMIT_SHORT_SHA
+if [ $CI_COMMIT_REF_NAME = "develop" ]; then
+  namespace='dev'
+  tag=$CI_COMMIT_REF_NAME
+elif [ ${CI_COMMIT_REF_NAME:0:1} = "v" ]; then
+  namespace='prod'
+  tag=$CI_COMMIT_REF_NAME
+fi
 IMAGE_TAG=${CI_BUILD_DOCKER_URL}mioyingtech/mio-go:$tag
+echo $IMAGE_TAG
 docker login -u $CI_BUILD_DOCKER_USER -p $CI_BUILD_TOKEN $CI_BUILD_DOCKER_URL
-docker build -t $IMAGE_TAG -f ./build/$1/Dockerfile ./cmd/$1
+docker build -t $IMAGE_TAG -f ./build/package/$1/Dockerfile ./cmd/$1
 docker push $IMAGE_TAG
 echo "package end"
