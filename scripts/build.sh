0eb715b9298e93c5b668f0bae6f1d3adee355b78
neiljin <neil.jin@miotech.com>
2022-04-14 13:23:42 +0800

    feat:删除分支时自动删除已部署的环境

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,6 @@
 # 参数1 要构建的目录名 mp2c
 echo "build start"
 cd $(dirname $(readlink -f "$0"))/../cmd/$1/
-pwd
+echo "build:$1 GOPROXY=https://goproxy.cn,https://goproxy.io,direct CGO_ENABLED=0 GOOS=linux GOARCH=amd64"
 env GOPROXY=https://goproxy.cn,https://goproxy.io,direct CGO_ENABLED=0 GOOS=linux GOARCH=amd64  go build -o $1 .
 echo "build end"
