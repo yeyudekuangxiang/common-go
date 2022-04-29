fd320e2c6a2ca56e9e63877203eb34ad36d51225
neiljin <neil.jin@miotech.com>
2022-04-29 10:09:52 +0800

    fix:修复积分筛选日期格式问题

--- internal/app/mp2c/controller/admin/types.go
+++ internal/app/mp2c/controller/admin/types.go
@@ -14,8 +14,8 @@ type GetPointRecordPageListFrom struct {
 	Nickname  string                      `json:"nickname" form:"nickname" binding:"lte=30" alias:"用户昵称"`
 	OpenId    string                      `json:"openId" form:"openId" binding:"lte=50" alias:"openId"`
 	Phone     string                      `json:"phone" form:"phone" binding:"lte=30" alias:"手机号"`
-	StartTime time.Time                   `json:"startTime" form:"startTime" alias:"开始时间" time_format:"2006-01-02 15:04:05" time_utc:"false" time_location:"Asia/Shanghai"`
-	EndTime   time.Time                   `json:"endTime" form:"endTime" alias:"结束时间" time_format:"2006-01-02 15:04:05" time_utc:"false" time_location:"Asia/Shanghai"`
+	StartTime time.Time                   `json:"startTime" form:"startTime" alias:"开始时间" time_format:"2006-01-02" time_utc:"false" time_location:"Asia/Shanghai"`
+	EndTime   time.Time                   `json:"endTime" form:"endTime" alias:"结束时间" time_format:"2006-01-02" time_utc:"false" time_location:"Asia/Shanghai"`
 	Type      entity.PointTransactionType `json:"type" form:"type" alias:"积分类型"`
 	controller.PageFrom
 }
