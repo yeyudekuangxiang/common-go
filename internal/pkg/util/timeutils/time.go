66893e047ca108a7c5b8b3eef6bd0855004f3cf4
neiljin <neil.jin@miotech.com>
2022-05-23 16:02:03 +0800

    feat:修复时间格式问题

--- internal/pkg/util/timeutils/time.go
+++ internal/pkg/util/timeutils/time.go
@@ -2,7 +2,7 @@ package timeutils
 
 import "time"
 
-const defaultTimeFormat = "2006-01-02 15:03:04"
+const defaultTimeFormat = "2006-01-02 15:04:05"
 const defaultDateFormat = "2006-01-02"
 
 func Now() Time {
