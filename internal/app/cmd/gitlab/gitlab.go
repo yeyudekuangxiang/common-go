085e4c782ace4a20979891e3ed25129f6a2cf2d1
neiljin <neil.jin@miotech.com>
2022-08-09 14:59:03 +0800

    feat:导入证书命令

--- internal/app/cmd/gitlab/gitlab.go
+++ internal/app/cmd/gitlab/gitlab.go
@@ -2,15 +2,16 @@
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 
 */
-package cmd
+package gitlab
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
 )
 
-// seedCmd represents the seed command
-var seedCmd = &cobra.Command{
-	Use:   "seed",
+// GitlabCmd represents the gitlab command
+var GitlabCmd = &cobra.Command{
+	Use:   "gitlab",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -19,20 +20,19 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-
+		fmt.Println("gitlab called")
 	},
 }
 
 func init() {
-	dbCmd.AddCommand(seedCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// seedCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// gitlabCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// seedCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// gitlabCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
