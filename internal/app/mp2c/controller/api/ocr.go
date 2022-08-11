25404edc7d1c5437a6b00b6c7961e17f839d1e53
yunfenglian <yunfeng.lian@miotech.com>
2022-08-11 12:09:41 +0800

    fix：orc接口限制

--- internal/app/mp2c/controller/api/ocr.go
+++ internal/app/mp2c/controller/api/ocr.go
@@ -3,7 +3,10 @@ package api
 import (
 	"github.com/gin-gonic/gin"
 	"mio/internal/pkg/service"
+	"mio/internal/pkg/util"
 	"mio/internal/pkg/util/apiutil"
+	"mio/pkg/errno"
+	"time"
 )
 
 var DefaultOCRController = OCRController{}
@@ -13,6 +16,12 @@ type OCRController struct {
 
 func (OCRController) GmTicket(c *gin.Context) (gin.H, error) {
 	user := apiutil.GetAuthUser(c)
+	if !util.DefaultLock.Lock("GmTicket:"+user.OpenId, time.Second*5) {
+		return nil, errno.ErrLimit.WithCaller()
+	}
+	if user.Risk > 2 {
+		return nil, errno.ErrCommon.WithMessage("无权限")
+	}
 	form := GetOCRForm{}
 	if err := apiutil.BindForm(c, &form); err != nil {
 		return nil, err
