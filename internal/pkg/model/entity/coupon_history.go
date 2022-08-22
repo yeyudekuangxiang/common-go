228983a0fb5b73fda5b6ed0f34d69b46f1233c53
yunfenglian <yunfeng.lian@miotech.com>
2022-08-22 09:46:32 +0800

    fix: 券类型暂无typeid

--- internal/pkg/model/entity/coupon_history.go
+++ internal/pkg/model/entity/coupon_history.go
@@ -5,13 +5,12 @@ import (
 )
 
 type CouponHistory struct {
-	ID           int64     `gorm:"primaryKey;autoIncrement" json:"id"`
-	OpenId       string    `gorm:"type:varchar(200)" json:"openId"`
-	CouponTypeId int64     `gorm:"type:varchar(100)" json:"couponTypeId"`
-	CouponType   string    `gorm:"type:varchar(50)" json:"couponType"`
-	Code         string    `gorm:"type:varchar(100)" json:"code"`
-	CreateTime   time.Time `json:"createTime"`
-	UpdateTime   time.Time `json:"updateTime"`
+	ID         int64     `gorm:"primaryKey;autoIncrement" json:"id"`
+	OpenId     string    `gorm:"type:varchar(200)" json:"openId"`
+	CouponType string    `gorm:"type:varchar(50)" json:"couponType"`
+	Code       string    `gorm:"type:varchar(100)" json:"code"`
+	CreateTime time.Time `json:"createTime"`
+	UpdateTime time.Time `json:"updateTime"`
 }
 
 func (CouponHistory) TableName() string {
