d489c3bd0c5fd4e1888c9369e7c24665054d0ae0
liumei <liumei@2345.com>
2022-08-25 14:54:07 +0800

    feat:碳成就

--- internal/app/mp2c/controller/api/point_collect.go
+++ internal/app/mp2c/controller/api/point_collect.go
@@ -35,7 +35,7 @@ func (PointCollectController) Collect(ctx *gin.Context) (gin.H, error) {
 		point, err = service.DefaultPointCollectService.CollectPowerReplace(user.OpenId, user.Risk, form.ImgUrl)
 	}
 	carbon := 0.0
-	if err != nil {
+	if err == nil {
 		//发碳量
 		carbon, err = service.NewCarbonTransactionService(context.NewMioContext()).Create(api_types.CreateCarbonTransactionDto{
 			OpenId:  user.OpenId,
