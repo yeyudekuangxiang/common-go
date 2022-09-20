7926e5ec4496eda021f3a2365411cbf2acc3145d
meiliu <mei.liu@miotech.com>
2022-09-20 14:44:44 +0800

    feat:小程序消息

--- config/message.go
+++ config/message.go
@@ -1,38 +1,11 @@
 package config
 
-//诸葛上报event_name
-type message struct {
-	Qnr                    string
-	UserLoginErr           string
-	UserLoginSuc           string
-	UserCertificateSendSuc string
-	UserIdentify           string
+//小程序订阅消息模版
+
+type MessageTemplateId struct {
+	SendPoint string
 }
 
-var messageStruct = message{
-	Qnr:                    "绿色金融调查问卷", // 金融调查问卷
-	UserLoginErr:           "用户登陆失败",   //用户登陆失败
-	UserLoginSuc:           "用户登陆成功",   //用户登陆成功
-	UserCertificateSendSuc: "携手同行-证书发放",
-	UserIdentify:           "用户渠道",
-}
-
-type MessageDataValueStruct struct {
-	Value string `json:"value"`
-}
-
-type MessageDataStruct struct {
-	Number1 MessageDataValueStruct `json:"number01"`
-	Thing5  MessageDataValueStruct `json:"date01"`
-	Time2   MessageDataValueStruct `json:"site01"`
-	Number3 MessageDataValueStruct `json:"site02"`
-}
-
-type MessageStruct struct {
-	Touser           string            `json:"touser"`
-	TemplateId       string            `json:"template_id"`
-	Page             string            `json:"page"`
-	MiniprogramState string            `json:"miniprogram_state"`
-	Lang             string            `json:"lang"`
-	Data             MessageDataStruct `json:"data"`
+var MessageTemplateIds = MessageTemplateId{
+	SendPoint: "hQzUwkGMYqgNsKOad7RnIwwBpfkVfsuJvW6UqymwI8k", // 金融调查问卷
 }
