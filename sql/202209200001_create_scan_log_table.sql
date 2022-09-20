060244f72665a05ba90ae9fff1202bc373a4b5b9
neiljin <neil.jin@miotech.com>
2022-09-20 17:33:08 +0800

    feat:添加扫描结果字段

--- sql/202209200001_create_scan_log_table.sql
+++ sql/202209200001_create_scan_log_table.sql
@@ -3,6 +3,7 @@ CREATE TABLE "public"."scan_log" (
                                 "image_url" varchar(1000) NOT NULL,
                                 "hash" varchar(255) NOT NULL,
                                 "count" int4 NOT NULL DEFAULT 0,
+                                "scan_result" varchar(1000) NOT NULL DEFAULT '',
                                 "created_at" timestamptz NOT NULL,
                                 "updated_at" timestamptz NOT NULL,
                                 PRIMARY KEY ("id"),
@@ -19,3 +20,5 @@ COMMENT ON COLUMN "public"."scan_log"."count" IS '扫描次数';
 COMMENT ON COLUMN "public"."scan_log"."created_at" IS '创建时间';
 
 COMMENT ON COLUMN "public"."scan_log"."updated_at" IS '更新时间';
+
+COMMENT ON COLUMN "public"."scan_log"."scan_result" IS '扫描结果';
