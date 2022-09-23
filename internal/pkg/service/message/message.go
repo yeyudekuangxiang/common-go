c171bfd702d2a3f478ef91b933f20845a4c83a62
meiliu <mei.liu@miotech.com>
2022-09-23 15:31:30 +0800

    feat:题目

--- internal/pkg/service/message/message.go
+++ internal/pkg/service/message/message.go
@@ -61,8 +61,10 @@ func (srv *MessageService) SendMiniSubMessage(toUser string, page string, templa
 		app.Logger.Info("小程序订阅消息发送失败，业务层，模版%s，toUser%s，错误信息%s", template.TemplateId(), toUser, ret.GetResponseError().Error())
 		return ret.ErrCode, ret.GetResponseError()
 	}
-	app.Redis.ZIncrBy(contextRedis.Background(), redisTemplateKey, 1, toUser).Err() //同一模板每人每天最多接收1条消息
-	app.Redis.ZIncrBy(contextRedis.Background(), redisUserKey, 1, toUser).Err()     //每人每天最多收到2个不同类型模板消息
+	if ret.ErrCode == 0 {
+		app.Redis.ZIncrBy(contextRedis.Background(), redisTemplateKey, 1, toUser).Err() //同一模板每人每天最多接收1条消息
+		app.Redis.ZIncrBy(contextRedis.Background(), redisUserKey, 1, toUser).Err()     //每人每天最多收到2个不同类型模板消息
+	}
 	return ret.ErrCode, nil
 }
 
