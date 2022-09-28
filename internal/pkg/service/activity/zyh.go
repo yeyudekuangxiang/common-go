b27c83f469e3be0d4a239bf0882d8f43e7808820
meiliu <mei.liu@miotech.com>
2022-09-28 15:45:28 +0800

    feat:志愿汇

--- internal/pkg/service/activity/zyh.go
+++ internal/pkg/service/activity/zyh.go
@@ -1,1 +1,31 @@
 package activity
+
+import (
+	"mio/internal/pkg/core/context"
+	repoActivity "mio/internal/pkg/repository/activity"
+)
+
+var DefaultAnswerService = ZyhService{ctx: context.NewMioContext()}
+
+func NewZyhService(ctx *context.MioContext) *ZyhService {
+	return &ZyhService{
+		ctx:     ctx,
+		zyhRepo: repoActivity.NewZyhRepository(ctx),
+	}
+}
+
+type ZyhService struct {
+	ctx     *context.MioContext
+	zyhRepo repoActivity.ZyhRepository
+}
+
+/*
+func (srv ZyhService) GetInfoBy(dto srv_types.DeleteQuestionAnswerDTO) error {
+	a := srv.zyhRepo.FindBy(repoActivity.FindZyhBy{
+		Openid: "111",
+		VolId:  "1111",
+	})
+	println(a)
+    return nil
+	//return a
+}*/
