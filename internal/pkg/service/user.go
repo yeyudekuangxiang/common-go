42e66392562ec8b2971c4287e45fe1ddc2b2974d
yunfenglian <yunfeng.lian@miotech.com>
2022-10-12 19:50:59 +0800

    fix: 绑定手机

--- internal/pkg/service/user.go
+++ internal/pkg/service/user.go
@@ -346,6 +346,7 @@ func (u UserService) BindPhoneByCode(userId int64, code string, cip string, invi
 	specialUser := DefaultUserSpecialService.GetSpecialUserByPhone(userInfo.PhoneNumber)
 	//检查重复绑定 特殊用户有已绑定的账号
 	if ok && userByMobile.OpenId != userInfo.OpenId && specialUser.ID == 0 {
+		app.Logger.Errorf("bind user: %s; old user: %s, isSpecial:%d", userInfo.OpenId, userByMobile.OpenId, specialUser.ID)
 		return errors.New("该号码已绑定")
 	}
 	//更新特殊用户的数据
