0ed02301c5c379b776b6407d22958f5e003e4da3
liumei <mei.liu@miotech.com>
2022-11-10 16:48:23 +0800

    金刚位

--- internal/pkg/repository/index_icon.go
+++ internal/pkg/repository/index_icon.go
@@ -88,7 +88,7 @@ func (repo IndexIconRepository) GetPage(do repotypes.GetIndexIconPageDO) ([]enti
 	if do.Title != "" {
 		db.Where("name like ?", "%"+do.Title+"%")
 	}
-	db.Order("id desc")
+	db.Order("sort desc ,id desc")
 	list := make([]entity.IndexIcon, 0)
 	var total int64
 	return list, total, db.Count(&total).Offset(do.Offset).Limit(do.Limit).Find(&list).Error
