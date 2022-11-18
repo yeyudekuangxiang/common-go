c375d33a632abe83e16a42e1b126c3777db826e5
liumei <mei.liu@miotech.com>
2022-11-18 14:29:34 +0800

    天津地铁

--- internal/pkg/service/platform/tianjin_metro/metro.go
+++ internal/pkg/service/platform/tianjin_metro/metro.go
@@ -32,39 +32,39 @@ type Service struct {
 }
 
 func (srv *Service) SendCoupon(typeId int64, amount float64, user entity.User) (string, error) {
-	// 用user加cid验证
+	//查询配置场景
+	bdScene := service.DefaultBdSceneService.FindByCh("tianjinmetro")
+	if bdScene.ID == 0 {
+		return "", errno.ErrNotFound
+	}
+
+	// 查询绑定关系或者注册关系
 	sceneUser := repository.DefaultBdSceneUserRepository.FindOne(repository.GetSceneUserOne{
 		PlatformKey: "tianjinmetro",
 		OpenId:      user.OpenId,
 	})
-
 	if sceneUser.PlatformUserId == "" {
 		app.Logger.Errorf("天津地铁 未找到绑定关系 : %s", user.OpenId)
 		return "", errno.ErrBindRecordNotFound
 	}
 
-	grantV5Request := MetroRequest{
+	//请求参数
+	Request := MetroRequest{
 		AllotId:     "33333333",
 		EtUserPhone: sceneUser.Phone,
 		AllotNum:    1,
 	}
-	//加密过程
-	jsons, errs := json.Marshal(grantV5Request) //转换成JSON返回的是byte[]
-	if errs != nil {
-		fmt.Println(errs.Error())
-	}
-	fmt.Println(string(jsons))
 
+	//加密过程
+	jsons, errs := json.Marshal(Request) //转换成JSON返回的是byte[]
+	if errs != nil {
+		return "", errs
+	}
 	str := Encode(string(jsons))
 	data, _ := encrypt.RsaEncrypt([]byte(str))
 	signature := base64.StdEncoding.EncodeToString(data)
 
-	bdScene := service.DefaultBdSceneService.FindByCh("tianjinmetro")
-
-	if bdScene.ID == 0 {
-		return "", errno.ErrNotFound
-	}
-
+	//header头
 	options := []httputil.HttpOption{
 		httputil.HttpWithHeader("appid", bdScene.AppId),
 		httputil.HttpWithHeader("random", ""),
@@ -73,8 +73,8 @@ func (srv *Service) SendCoupon(typeId int64, amount float64, user entity.User) (
 	}
 
 	url := bdScene.Domain + "/tj-metro-api/open-forward/api/eTicket/allot"
-	body, err := httputil.PostJson(url, grantV5Request, options...)
-	app.Logger.Infof("天津地铁 grantV2 返回 : %s", body)
+	body, err := httputil.PostJson(url, Request, options...)
+	app.Logger.Infof("天津地铁 返回 : %s", body)
 	if err != nil {
 		return "", err
 	}
@@ -82,12 +82,12 @@ func (srv *Service) SendCoupon(typeId int64, amount float64, user entity.User) (
 	response := MetroResponse{}
 	err = json.Unmarshal(body, &response)
 	if err != nil {
-		app.Logger.Errorf("天津地铁 grantV2 json_decode_err: %s", err.Error())
+		app.Logger.Errorf("天津地铁 json_decode_err: %s", err.Error())
 		return "", err
 	}
 
 	if response.SubCode != "0000" {
-		app.Logger.Errorf("天津地铁 grantV2 err: %s\n", response.SubMessage)
+		app.Logger.Errorf("天津地铁 err: %s\n", response.SubMessage)
 		return "", errors.New(response.SubMessage)
 	}
 	//记录
@@ -104,7 +104,6 @@ func (srv *Service) SendCoupon(typeId int64, amount float64, user entity.User) (
 		app.Logger.Errorf("天津地铁 券包 发放错误 : %s\n", err.Error())
 		return "", err
 	}
-
 	return response.SubData.OrderNo, nil
 }
 
