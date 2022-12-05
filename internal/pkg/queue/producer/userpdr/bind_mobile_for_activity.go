be06b529681ae7f9e4447515e4fba3266be11a26
yunfenglian <yunfeng.lian@miotech.com>
2022-12-05 18:12:51 +0800

    fix: return nil

--- internal/pkg/queue/producer/userpdr/bind_mobile_for_activity.go
+++ internal/pkg/queue/producer/userpdr/bind_mobile_for_activity.go
@@ -1,11 +1,20 @@
 package userpdr
 
-//func BindMobileForActivity(msg usermsg.BindMobile) error {
-//	marshal, err := json.Marshal(msg)
-//	if err != nil {
-//		return err
-//	}
-//	return app.QueueProduct.Publish(marshal,
-//		[]string{routerkey.BindMobileForActivity},
-//		rabbitmq.WithPublishOptionsExchange("userExchange"))
-//}
+import (
+	"encoding/json"
+	"github.com/wagslane/go-rabbitmq"
+	"mio/internal/pkg/core/app"
+	"mio/internal/pkg/queue/types/message/usermsg"
+	"mio/internal/pkg/queue/types/routerkey"
+)
+
+func BindMobileForActivity(msg usermsg.BindMobile) error {
+	return nil
+	marshal, err := json.Marshal(msg)
+	if err != nil {
+		return err
+	}
+	return app.QueueProduct.Publish(marshal,
+		[]string{routerkey.BindMobileForActivity},
+		rabbitmq.WithPublishOptionsExchange("userExchange"))
+}
