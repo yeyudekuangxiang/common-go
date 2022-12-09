ce5e8053b8bd5bbc6c82082d74bc1e5806ed9df7
neiljin <neil.jin@miotech.com>
2022-12-09 14:42:16 +0800

    feat:mp2c-go access接入prometheus

--- internal/pkg/core/initialize/prom.go
+++ internal/pkg/core/initialize/prom.go
@@ -2,13 +2,18 @@ package initialize
 
 import (
 	"github.com/zeromicro/go-zero/core/prometheus"
+	"log"
 	"mio/config"
 )
 
 func InitProm() {
-	prometheus.StartAgent(prometheus.Config{
-		Host: config.Config.Prometheus.Host,
-		Port: config.Config.Prometheus.Port,
-		Path: config.Config.Prometheus.Path,
-	})
+	if len(config.Config.Prometheus.Host) != 0 {
+		log.Println("初始化prometheus...")
+		prometheus.StartAgent(prometheus.Config{
+			Host: config.Config.Prometheus.Host,
+			Port: config.Config.Prometheus.Port,
+			Path: config.Config.Prometheus.Path,
+		})
+		log.Println("初始化prometheus成功")
+	}
 }
