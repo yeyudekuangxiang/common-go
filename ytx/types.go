618bf0c23b78a0834d8a811788241b28c15d4b20
yunfenglian <yunfeng.lian@miotech.com>
2023-03-27 14:54:19 +0800

    feat: 亿通行、金华行发券逻辑

---
+++ ytx/types.go
@@ -0,0 +1,38 @@
+package ytx
+
+type GrantV2Request struct {
+	AppId       string         `json:"appId"`
+	AppSecret   string         `json:"appSecret"`
+	Ts          string         `json:"ts"`
+	Version     string         `json:"version,omitempty"`
+	AppVersion  string         `json:"appVersion,omitempty"`
+	DeviceType  string         `json:"deviceType,omitempty"`
+	DeviceId    string         `json:"deviceId,omitempty"`
+	RedirectUrl string         `json:"redirectUrl,omitempty"`
+	OpenId      string         `json:"openId,omitempty"`
+	UserId      string         `json:"userId,omitempty"`
+	ReqData     GrantV2ReqData `json:"reqData"`
+}
+
+type GrantV2ReqData struct {
+	OrderNo  string  `json:"orderNo"`
+	PoolCode string  `json:"poolCode"`
+	Amount   float64 `json:"amount"`
+	UserId   string  `json:"userId,omitempty"`
+	PhoneNum string  `json:"phoneNum,omitempty"`
+	OpenId   string  `json:"openId,omitempty"`
+	Remark   string  `json:"remark"`
+}
+
+type GrantV2Response struct {
+	SubCode    string         `json:"subCode,omitempty"`
+	SubData    GrantV2SubData `json:"subData"`
+	SubMessage string         `json:"subMessage,omitempty"`
+}
+
+type GrantV2SubData struct {
+	OrderNo     string `json:"orderNo"`
+	TradeNo     string `json:"tradeNo,omitempty"`
+	Success     bool   `json:"success"`
+	Description string `json:"description,omitempty"`
+}
