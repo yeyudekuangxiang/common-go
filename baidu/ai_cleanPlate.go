1928f49219e280a8a2354c653c3ba94332fe7531
yflian <yunfeng.lian@miotech.com>
2023-04-26 10:25:53 +0800

    feat: 百度新增ai光盘识图

---
+++ baidu/ai_cleanPlate.go
@@ -0,0 +1,53 @@
+package baidu
+
+import (
+	"encoding/json"
+	"fmt"
+	"gitlab.miotech.com/miotech-application/backend/common-go/tool/httptool"
+)
+
+const (
+	aiCleanPlateUrl = "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/classification/emptyPlateRecognize"
+)
+
+type AiCleanPlateClient struct {
+	AccessToken IAccessToken
+}
+
+func NewAiCleanPlateClient(accessToken IAccessToken) *AiCleanPlateClient {
+	return &AiCleanPlateClient{
+		AccessToken: accessToken,
+	}
+}
+
+type CleanPlateParam struct {
+	ImageUrl string
+}
+
+type CleanPlateResult struct {
+	CommonRespCode
+	LogId   int64 `json:"log_id"`
+	Results []struct {
+		Name  string `json:"name"`
+		Score int64  `json:"score"`
+	} `json:"results"`
+}
+
+func (c *AiCleanPlateClient) CleanPlate(param CleanPlateParam) (*CleanPlateResult, error) {
+	token, err := c.AccessToken.GetToken()
+	if err != nil {
+		return nil, err
+	}
+	u := fmt.Sprintf("%s?access_token=%s&input_type=%s", aiCleanPlateUrl, token, "url")
+	body, err := httptool.PostMapFrom(u, map[string]string{
+		"url": param.ImageUrl,
+	})
+	if err != nil {
+		return nil, err
+	}
+	resp := CleanPlateResult{}
+	if err := json.Unmarshal(body, &resp); err != nil {
+		return nil, err
+	}
+	return &resp, nil
+}
