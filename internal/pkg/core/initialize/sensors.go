547b41c52f5fe03cb6f3db72514ee899252d923f
meiliu <mei.liu@miotech.com>
2023-05-09 15:42:51 +0800

    神策埋点-异步

--- internal/pkg/core/initialize/sensors.go
+++ internal/pkg/core/initialize/sensors.go
@@ -12,44 +12,32 @@ import (
 func InitSensors() {
 	if config.Config.Sensors.SaServerUrl != "" {
 		log.Println("开始初始化神策全局客户端...")
+		if config.Config.Sensors.Debug {
+			// 初始化 Debug Consumer
+			consumer, err := sdk.InitDebugConsumer(config.Config.Sensors.SaServerUrl, config.Config.Sensors.Debug, config.Config.Sensors.SaRequestTimeout)
+			if err != nil {
+				return
+			}
+			// 使用 Consumer 来构造 SensorsAnalytics 对象
+			sa := sdk.InitSensorsAnalytics(consumer, "default", false)
+			defer sa.Close()
 
-		// 初始化 ConcurrentLoggingConsumer
-
-		// 初始化一个 Consumer，用于数据发送
-		consumer, err := sdk.InitConcurrentLoggingConsumer(config.Config.Sensors.SaServerUrl, true)
-		//...
-		if err != nil {
-			return
+			*app.SensorsClient = sa
+		} else {
+			//生产用
+			consumer, err := sdk.InitDefaultConsumer(config.Config.Sensors.SaServerUrl, config.Config.Sensors.SaRequestTimeout)
+			if err != nil {
+				return
+			}
+			// 使用 Consumer 来构造 SensorsAnalytics 对象
+			sa := sdk.InitSensorsAnalytics(consumer, "default", false)
+			defer sa.Close()
+			*app.SensorsClient = sa
 		}
-		// ...
-		// 使用 Consumer 来构造 SensorsAnalytics 对象
-		sa := sdk.InitSensorsAnalytics(consumer, "default", false)
 
-		// 程序结束前调用 Close() ，让 Consumer 刷新所有缓存数据到文件中
-		defer sa.Close()
-
-		/*// 初始化 Debug Consumer
-		consumer, err := sdk.InitDebugConsumer(config.Config.Sensors.SaServerUrl, config.Config.Sensors.Debug, config.Config.Sensors.SaRequestTimeout)
-		if err != nil {
-			return
-		}
-		// 使用 Consumer 来构造 SensorsAnalytics 对象
-		sa := sdk.InitSensorsAnalytics(consumer, "default", false)
-		defer sa.Close()
-		*/
-		*app.SensorsClient = sa
 		log.Println("初始化全局神策客户端成功...")
-
-		zhuGeAttr := make(map[string]interface{}, 0) //诸葛打点
-		zhuGeAttr["shop"] = "1"
-		err = app.SensorsClient.Track("37473t374t3", "sgsdgdggsd", zhuGeAttr, false)
-		if err != nil {
-			println("11212")
-		}
-		println("44444")
-
 		/*
-			guid := "qwqw121212"
+			guid := "test"
 			properties := map[string]interface{}{
 				"price": 12,
 				"name":  "apple",
@@ -66,7 +54,7 @@ func InitSensors() {
 			}
 		*/
 
-		/*guid := "qwqw121212"
+		/*guid := "testd"
 		properties := map[string]interface{}{
 			"price": 12,
 			"name":  "apple",
