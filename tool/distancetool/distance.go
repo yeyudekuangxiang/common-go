aed42ed3081fcd64f737f820105467d50660293a
meiliu <mei.liu@miotech.com>
2023-06-01 15:04:26 +0800

    计算距离

---
+++ tool/distancetool/distance.go
@@ -0,0 +1,76 @@
+package distancetool
+
+import (
+	"github.com/pkg/errors"
+	"math"
+	"sort"
+)
+
+const earthRadius = 6371000 // 地球赤道半径，单位米
+
+// 点的结构体，包含经纬度和点的名称
+type point struct {
+	name string
+	lat  float64
+	lng  float64
+}
+
+// 用于计算距离的函数
+func distance(p1, p2 point) float64 {
+	deltaLat := toRadians(p2.lat - p1.lat)
+	deltaLng := toRadians(p2.lng - p1.lng)
+	a := math.Sin(deltaLat/2)*math.Sin(deltaLat/2) +
+		math.Cos(toRadians(p1.lat))*math.Cos(toRadians(p2.lat))*
+			math.Sin(deltaLng/2)*math.Sin(deltaLng/2)
+
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+	return earthRadius * c
+}
+
+// 将角度转换为弧度
+func toRadians(degrees float64) float64 {
+	return degrees * math.Pi / 180
+}
+
+func Distance(origin point, targets []point) (resultSlice, error) {
+	if len(targets) == 0 {
+		return nil, errors.New("参数有误")
+	}
+	// 计算距离并存储结果到resultSlice
+	var results resultSlice
+	for _, target := range targets {
+		d := distance(origin, target)
+		r := result{distance: d, point: target}
+		results = append(results, r)
+	}
+	// 按距离升序排序
+	sort.Sort(results)
+	return results, nil
+}
+
+// 结果项的结构体，包含距离和点的信息
+type result struct {
+	distance float64
+	point    point
+}
+
+// 定义结果项列表，用于存储每个点与起点之间的距离
+type resultSlice []result
+
+// 实现sort.Interface接口的Len方法
+
+func (rs resultSlice) Len() int {
+	return len(rs)
+}
+
+// 实现sort.Interface接口的Swap方法
+
+func (rs resultSlice) Swap(i, j int) {
+	rs[i], rs[j] = rs[j], rs[i]
+}
+
+// 实现sort.Interface接口的Less方法，按距离升序排列
+
+func (rs resultSlice) Less(i, j int) bool {
+	return rs[i].distance < rs[j].distance
+}
---
+++ tool/distancetool/distance_test.go
@@ -0,0 +1,71 @@
+package distancetool
+
+import (
+	"fmt"
+	"testing"
+)
+
+// 实现分页方法
+func (rs resultSlice) Page(page int, pageSize int) []result {
+	start := (page - 1) * pageSize
+	end := start + pageSize
+	if end > len(rs) {
+		end = len(rs)
+	}
+	if start > end {
+		start = end
+	}
+	return rs[start:end]
+}
+
+func TestDis(t *testing.T) {
+	// 起点
+	origin := point{lat: 31.23, lng: 121.47}
+
+	// 目标点列表
+	var targets []point = []point{
+		{name: "北京", lat: 39.90, lng: 116.41},
+		{name: "香港", lat: 22.27, lng: 114.17},
+		{name: "广州", lat: 23.13, lng: 113.27},
+		{name: "南京", lat: 32.05, lng: 118.78},
+		{name: "上海", lat: 31.23, lng: 121.47},
+		{name: "深圳", lat: 22.54, lng: 114.06},
+		{name: "福州", lat: 26.08, lng: 119.30},
+	}
+
+	slice, err := Distance(origin, targets)
+	if err != nil {
+		return
+	}
+	/*for _, r := range slice {
+		fmt.Printf("%-4s距离 %.2f 米\n", r.point.name, r.distance)
+	}*/
+
+	// 分页
+	pageSize := 2
+	pageCount := (len(slice) + pageSize - 1) / pageSize
+	for i := 1; i <= pageCount; i++ {
+		pageResults := slice.Page(i, pageSize)
+		fmt.Printf("第 %d 页：\n", i)
+		for _, r := range pageResults {
+			fmt.Printf("%-4s距离 %.2f 米\n", r.point.name, r.distance*1000)
+		}
+	}
+	/*
+		// 计算距离并存储结果到resultSlice
+		var results resultSlice
+		for _, target := range targets {
+			d := distance(origin, target)
+			r := result{distance: d, point: target}
+			results = append(results, r)
+		}
+
+		// 按距离升序排序
+		sort.Sort(results)
+
+		// 输出结果
+		for _, r := range results {
+			fmt.Printf("%-4s距离 %.2f 米\n", r.point.name, r.distance)
+		}*/
+
+}
