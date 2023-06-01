e1e637b8da7e413aa60c61d7977cf01efb37c6d3
meiliu <mei.liu@miotech.com>
2023-06-01 15:22:54 +0800

    计算距离

--- tool/distancetool/distance.go
+++ tool/distancetool/distance.go
@@ -11,18 +11,18 @@ const earthRadius = 6371000 // 地球赤道半径，单位米
 // 点的结构体，包含经纬度和点的名称
 
 type Point struct {
-	name string
-	lat  float64
-	lng  float64
+	Name string
+	Lat  float64
+	Lng  float64
 }
 
 // 用于计算距离的函数
 
 func Distance(p1, p2 Point) float64 {
-	deltaLat := toRadians(p2.lat - p1.lat)
-	deltaLng := toRadians(p2.lng - p1.lng)
+	deltaLat := toRadians(p2.Lat - p1.Lat)
+	deltaLng := toRadians(p2.Lng - p1.Lng)
 	a := math.Sin(deltaLat/2)*math.Sin(deltaLat/2) +
-		math.Cos(toRadians(p1.lat))*math.Cos(toRadians(p2.lat))*
+		math.Cos(toRadians(p1.Lat))*math.Cos(toRadians(p2.Lat))*
 			math.Sin(deltaLng/2)*math.Sin(deltaLng/2)
 
 	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
@@ -42,7 +42,7 @@ func DistanceArr(origin Point, targets []Point) (resultSlice, error) {
 	var results resultSlice
 	for _, target := range targets {
 		d := Distance(origin, target)
-		r := result{distance: d, point: target}
+		r := result{Distance: d, Point: target}
 		results = append(results, r)
 	}
 	// 按距离升序排序
@@ -52,8 +52,8 @@ func DistanceArr(origin Point, targets []Point) (resultSlice, error) {
 
 // 结果项的结构体，包含距离和点的信息
 type result struct {
-	distance float64
-	point    Point
+	Distance float64
+	Point    Point
 }
 
 // 定义结果项列表，用于存储每个点与起点之间的距离
@@ -74,5 +74,5 @@ func (rs resultSlice) Swap(i, j int) {
 // 实现sort.Interface接口的Less方法，按距离升序排列
 
 func (rs resultSlice) Less(i, j int) bool {
-	return rs[i].distance < rs[j].distance
+	return rs[i].Distance < rs[j].Distance
 }
