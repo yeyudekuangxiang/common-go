19513c2c22f20de8fcfae18e13e83154983cb6e8
meiliu <mei.liu@miotech.com>
2023-08-10 17:11:50 +0800

    积分抵扣服务费

--- order/unit.go
+++ order/unit.go
@@ -4,18 +4,18 @@ import "github.com/shopspring/decimal"
 
 // 检测是否有使用过该优惠
 
-func PointToMoneyFen(point int64, rate int64) int64 {
+func PointToMoneyFen(point uint64, rate int64) int64 {
 	if point == 0 || rate == 0 {
 		return 0
 	}
-	pointDec := decimal.NewFromInt(point)
+	pointDec := decimal.NewFromInt(int64(point))
 	return pointDec.Div(decimal.NewFromInt(rate).Round(2)).Mul(decimal.NewFromInt(100)).BigInt().Int64()
 }
 
 // 设置使用过的优惠
 
-func PointToMoneyYuan(point int64, rate int64) float64 {
-	a := PointToMoneyFen(point, rate)
-	c, _ := decimal.NewFromInt(a).Div(decimal.NewFromInt(100)).Round(2).Float64()
-	return c
+func PointToMoneyYuan(point uint64, rate int64) float64 {
+	moneyFen := PointToMoneyFen(point, rate)
+	money, _ := decimal.NewFromInt(moneyFen).Div(decimal.NewFromInt(100)).Round(2).Float64()
+	return money
 }
