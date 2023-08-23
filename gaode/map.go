e3c58e86abb699a22a5f3d15c9a3c69d7af4306a
meiliu <mei.liu@miotech.com>
2023-08-23 16:12:48 +0800

    高德ip服务

--- gaode/map.go
+++ gaode/map.go
@@ -2,7 +2,9 @@ package gaode
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"gitlab.miotech.com/miotech-application/backend/common-go/baidu"
 	"gitlab.miotech.com/miotech-application/backend/common-go/tool/httptool"
 )
 
@@ -31,6 +33,72 @@ func (c *MapClient) LocationIp(ip string) (*LocationIpResult, error) {
 
 type LocationIpResult struct {
 	Status    string `json:"status"`
+	Info      string `json:"info"`
+	Infocode  string `json:"infocode"`
+	Province  string `json:"province"`
+	City      string `json:"city"`
+	Adcode    string `json:"adcode"`
+	Rectangle string `json:"rectangle"`
+}
+type MapWithBaiduClient struct {
+	GaoDeAk string
+	BaiDuAk string
+}
+
+//优先使用高德,高德查不到，再使用百度
+
+func NewMapWithBaiduClient(gaoDeAk string, baiduAk string) *MapWithBaiduClient {
+	return &MapWithBaiduClient{GaoDeAk: gaoDeAk, BaiDuAk: baiduAk}
+}
+
+func (c *MapWithBaiduClient) LocationIp(ip string) (*LocationIpResultV2, error) {
+	locationIp, err := NewMapClient(c.GaoDeAk).LocationIp(ip)
+	if err != nil {
+		return nil, err
+	}
+	if locationIp.Status == "1" && locationIp.Province != "" {
+		return &LocationIpResultV2{
+			StatusRespCode: StatusRespCode{
+				Status: locationIp.Status,
+			},
+			Info:      locationIp.Info,
+			Infocode:  locationIp.Infocode,
+			Province:  locationIp.Province,
+			City:      locationIp.City,
+			Adcode:    locationIp.Adcode,
+			Rectangle: locationIp.Rectangle,
+		}, nil
+	}
+	result, err := baidu.NewMapClient(c.BaiDuAk).LocationIp(ip)
+	if err != nil {
+		return nil, err
+	}
+	if result.IsSuccess() {
+		return &LocationIpResultV2{
+			StatusRespCode: StatusRespCode{
+				Status: "1",
+			},
+			Info:      "ok",
+			Infocode:  "",
+			Province:  result.Content.AddressDetail.Province,
+			City:      result.Content.AddressDetail.City,
+			Adcode:    result.Content.AddressDetail.Adcode,
+			Rectangle: "",
+		}, nil
+	}
+	return nil, errors.New("ip定位失败")
+}
+
+type StatusRespCode struct {
+	Status string `json:"status"`
+}
+
+func (result StatusRespCode) IsSuccess() bool {
+	return result.Status == "1"
+}
+
+type LocationIpResultV2 struct {
+	StatusRespCode
 	Info      string `json:"info"`
 	Infocode  string `json:"infocode"`
 	Province  string `json:"province"`
